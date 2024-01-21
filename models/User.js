6cb4675648e8323cf88554bac6603d3433119992
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2024-01-21 11:57:27 +0530

    changed multer

--- models/User.js
+++ models/User.js
@@ -8,12 +8,12 @@ const UserSchema = new mongoose.Schema({
        required:true,
        trim:true
     },
-    img:{
-        type:String,
-        // required:true,
-        
-        trim:true
-    }
+    // img:{
+    //     type:String,
+    //     // required:true,
+
+    //     trim:true
+    // }
 
 },{timestamps:true})
 
